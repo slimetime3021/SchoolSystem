f2d1245f8bdc691b0aac5248e57b7bea6652b732
derick <derick.burjack.cardoso@gmail.com>
2025-12-06 22:16:33 -0500

    Added generate random score method for assignment class

--- src/main/java/org/derick/Assignment.java
+++ src/main/java/org/derick/Assignment.java
@@ -1,6 +1,7 @@
 package org.derick;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Assignment {
     String assignmentId;
@@ -14,5 +15,19 @@ public class Assignment {
         this.weight = weight;
         this.scores = new ArrayList<>();
         this.assignmentId = String.format("%06d", nextId++);
+    }
+
+    void generateRandomScore() {
+        Random rand = new Random();
+        int num = rand.nextInt(0,11);
+        Integer score = switch (num){
+            case 0 -> rand.nextInt(0,60);
+            case 1, 2 -> rand.nextInt(60,70);
+            case 3, 4 -> rand.nextInt(70,80);
+            case 5, 6, 7, 8 -> rand.nextInt(80,90);
+            case 9, 10 -> rand.nextInt(90,100);
+            default -> -1;
+        };
+        scores.add(score);
     }
 }
