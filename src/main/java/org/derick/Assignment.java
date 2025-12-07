3f36d8b4e8c3971254fd18fba4d6e18f07abd0fa
derick <derick.burjack.cardoso@gmail.com>
2025-12-06 22:06:15 -0500

    Added the fields and a constructor with the weight and assignment name for the assignment class

--- src/main/java/org/derick/Assignment.java
+++ src/main/java/org/derick/Assignment.java
@@ -1,4 +1,18 @@
 package org.derick;
 
+import java.util.ArrayList;
+
 public class Assignment {
+    String assignmentId;
+    String assignmentName;
+    double weight;
+    ArrayList<Integer> scores;
+    static int nextId = 1;
+
+    public Assignment(String assignmentName, double weight) {
+        this.assignmentName = assignmentName;
+        this.weight = weight;
+        this.scores = new ArrayList<>();
+        this.assignmentId = String.format("%06d", nextId++);
+    }
 }
