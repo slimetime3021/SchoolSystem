12c66a673b7173b68b6ad725d013cb4c4884a42b
derick <derick.burjack.cardoso@gmail.com>
2025-12-12 21:20:44 -0500

    Added To string, equals and getters for Department class

--- src/main/java/org/derick/Department.java
+++ src/main/java/org/derick/Department.java
@@ -1,12 +1,27 @@
 package org.derick;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Department {
     private String departmentId;
     private String departmentName;
     private static int nextId = 1;
 
-
-
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D-%02d",  nextId++);
+            this.departmentName = departmentName;
+        }
+        else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 
     static boolean isDepartmentNameValid(String departmentName) {
         for (int i = 0; i < departmentName.length(); i++) {
