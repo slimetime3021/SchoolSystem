3cb3d9f93eae89cec677bbdc69753ee253161210
derick <derick.burjack.cardoso@gmail.com>
2025-12-12 21:32:32 -0500

    Added fields for Course class

--- src/main/java/org/derick/Course.java
+++ src/main/java/org/derick/Course.java
@@ -1,5 +1,13 @@
 package org.derick;
 
-public class Course {
+import java.util.ArrayList;
 
+public class Course {
+    String courseId;
+    String courseName;
+    double credits;
+    Department department;
+    ArrayList<Assignment> assignments;
+    ArrayList<Student> registeredStudents;
+    static int nextId = 1;
 }
